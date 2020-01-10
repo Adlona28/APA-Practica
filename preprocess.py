7e27a4d89d8beb043f6854bd80f67c79c6987e65
alexiniesta7 <iniestalex.garcia@hotmail.com>
2020-01-11 00:03:12 +0100

    Starting preprocess data

---
+++ preprocess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 22:33:33 2020
+
+@author: alex
+"""
+
+import csv
+import numpy as np
+import time
+
+def gendermap(s):
+    if s == 'M':
+        return [1,0,0]
+    elif s == 'F':
+        return [0,1,0]
+    else:
+        return [0,0,1]
+
+inputs = []
+outputs = []
+'''
+1. As we need a numerical representation for the first variable, gender,
+we can do a three positions one hot encoding, which M will be
+[1,0,0], for F, [0,1,0] and for I, [0,0,1].
+'''
+with open('abalone.csv') as data:
+    data = csv.reader(data, delimiter=',')
+    for abalone in data:
+        if len(abalone) > 0:
+            entrada = gendermap(abalone[0])
+            entrada.extend(list(map(lambda x: float(x), abalone[1:-1])))
+            inputs.append(entrada)
+            outputs.append(int(abalone[-1]))
+
+print(inputs, len(inputs))
