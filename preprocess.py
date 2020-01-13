e2ddecce0b2d5b22ada8fbaee9082d3ca4d105f4
adlona28 <adrian.lozano.navarro@est.fib.upc.edu>
2020-01-13 17:07:15 +0100

    pre done

--- preprocess.py
+++ preprocess.py
@@ -2,15 +2,16 @@
 Name / Data Type / Measurement Unit / Description
 -----------------------------
 Sex / nominal / -- / M, F, and I (infant)
-Length / continuous / mm / Longest shell measurement
-Diameter / continuous / mm / diam
-Height / continuous / mm / with meat in shell
+length / continuous / mm / Longest shell measurement
+diam / continuous / mm / diam
+height / continuous / mm / with meat in shell
 Whole weight / continuous / grams / whole abalone
 Shucked weight / continuous / grams / weight of meat
 Viscera weight / continuous / grams / gut weight (after bleeding)
 Shell weight / continuous / grams / after being dried
-Rings / integer / -- / +1.5 gives the age in years
+rings / integer / -- / +1.5 gives the age in years
 """
+
 import csv
 import numpy as np
 import time
@@ -18,94 +19,267 @@ import seaborn as sns
 import pandas as pd 
 import matplotlib.pyplot as plt
 from scipy import stats
+from pandas.api.types import is_numeric_dtype
+from sklearn.preprocessing import  StandardScaler
+from sklearn.model_selection import  train_test_split, cross_val_score
+from sklearn.feature_selection import SelectKBest
+from sklearn import preprocessing
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
-def grafiques(abalone_df):
-    
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['length'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Longest shell measurement')
-    fig.savefig('./graficas/lengthScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['diam'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Diameter (mm)')
-    fig.savefig('./graficas/diamScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['height'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Height (mm)')
-    fig.savefig('./graficas/heightScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['wholew'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Weight (grams)')
-    fig.savefig('./graficas/wholewScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['shuckedw'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Shucked weight (grams)')
-    fig.savefig('./graficas/shuckedwScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['visceraw'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Gut weight (mm)')
-    fig.savefig('./graficas/viscerawScatter.png')
- 
-    fig, ax = plt.subplots(figsize=(16,8))
-    ax.scatter(abalone_df['rings'], abalone_df['shellw'])
-    ax.set_xlabel('Rings')
-    ax.set_ylabel('Shell weight (mm)')
-    fig.savefig('./graficas/shellwScatter.png')
+def grafiques(abalone):    
+    sns.set()
+
+    abalone = pd.read_csv('abalone.csv', sep=',')
+    abalone.head()
+    abalone.describe()
+
+    rows = 2
+    cols = 2
+    i = 0
+
+    plt.figure(figsize=(cols * 5, rows * 5))
+
+    i += 1
+    plt.subplot(rows, cols, i)
+    plt.xticks(range(0, 31, 4))
+    plt.xlim(0, 30)
+    _ = sns.distplot(abalone['rings'], kde=False, bins=range(0, 31, 2))
+
+    i += 1
+    plt.subplot(rows, cols, i)
+    _ = sns.distplot(abalone['rings'])
+
+    i += 1
+    plt.subplot(rows, cols, i)
+    plt.xticks(range(0, 31, 4))
+    plt.xlim(0, 30)
+    _ = sns.boxplot(abalone['rings'])
+    plt.savefig('./graficas/rings.png')
+
+    plt.figure(figsize=(15, 15))
+
+    colors = sns.color_palette()
+
+    lines = 3
+    rows = 3
+    i = 0
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['length'], color=colors[i % 3])
+        
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['diam'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['height'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['length'], kde=False, bins=np.arange(0.0, 0.9, 0.05), color=colors[i % 3])
+        
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['diam'], kde=False, bins=np.arange(0.0, 0.7, 0.05), color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['height'], kde=False, bins=10, color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['length'], color=sns.color_palette()[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['diam'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['height'], color=colors[i % 3])
+
+    plt.savefig('./graficas/sizes.png')
+
+    abalone = abalone[abalone['height'] < 0.4]
+
+    plt.figure(figsize=(15, 15))
+
+    colors = sns.color_palette()
+
+    lines = 3
+    rows = 3
+    i = 0
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['length'], color=colors[i % 3])
+        
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['diam'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['height'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['length'], kde=False, bins=np.arange(0.0, 0.9, 0.05), color=colors[i % 3])
+        
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['diam'], kde=False, bins=np.arange(0.0, 0.7, 0.05), color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.distplot(abalone['height'], kde=False, bins=10, color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['length'], color=sns.color_palette()[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['diam'], color=colors[i % 3])
+
+    i += 1
+    plt.subplot(lines, rows, i)
+    _ = sns.boxplot(abalone['height'], color=colors[i % 3])
+
+    plt.savefig('./graficas/sizesWithoutOutliers.png')
+
+    plt.figure(figsize=(10, 10))
+    corr = abalone.corr()
+    _ = sns.heatmap(corr, annot=True)
+
+    plt.savefig('./graficas/correlation.png')
+
+    i_abalone = abalone[abalone['rings'] < 10]
+    plt.figure(figsize=(10, 10))
+    corr = i_abalone.corr()
+    _ = sns.heatmap(corr, annot=True)
+
+    plt.savefig('./graficas/correlation<10.png')
+
+    a_abalone = abalone[abalone['rings'] >= 10]
+    plt.figure(figsize=(10, 10))
+    corr = a_abalone.corr()
+    _ = sns.heatmap(corr, annot=True)
+
+    plt.savefig('./graficas/correlation>10.png')
 
 def elimOutliers(abalone_df):
     z = np.abs(stats.zscore(abalone_df))
     indexes = np.where(z > 3)
     return dataset.drop(indexes[0])
 
-def createTestAndTrain(partToTrain, dataset):
+def createTestAndTrain(partToTest, dataset):
+    
+    """
     msk = np.random.rand(len(dataset)) < partToTrain
     train = dataset[msk]
     test = dataset[~msk]
-    train.to_csv('train.csv')
-    test.to_csv('test.csv')
+    """
+    X = dataset.drop('rings', axis = 1)
+    y = dataset['rings']
+    standardScale = StandardScaler()
+    standardScale.fit_transform(X)
+    selectkBest = SelectKBest()
+    X_new = selectkBest.fit_transform(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size = partToTest)
+    
 
-    with open("train.csv", "r") as f:
-        lines = f.readlines()
+    df1 = pd.DataFrame(X_train)
+    x = df1.values 
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df1 = pd.DataFrame(x_scaled)
+
+    df2 = pd.DataFrame(y_train)
+    
+
+    df3 = pd.DataFrame(X_test)
+    x = df3.values 
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df3 = pd.DataFrame(x_scaled)
+
+    df4 = pd.DataFrame(y_test)
+
+    
+    df1.to_csv('df1.csv')
+    df2.to_csv('df2.csv')
+    df3.to_csv('df3.csv')
+    df4.to_csv('df4.csv')
+
+    with open("df1.csv", "r") as f:
+        lines1 = f.readlines()
+    with open("df2.csv", "r") as f:
+        lines2 = f.readlines()
     with open("train.csv", "w") as f:
-        for line in lines:
-            if line.strip("\n") != ",sex1,sex2,sex3,length,diam,height,wholew,shuckedw,visceraw,shellw,rings":
-                borrar = True
-                fline = ''
-                for i in line:
-                    if borrar:
-                        if i == ',':
-                            borrar = False
-                    else:
-                        fline = fline + i
-                f.write(fline)
+        for i in range(len(lines1)):
+            if lines1[i].strip("\n") != ",0,1,2,3,4,5,6,7,8,9":
+                borrar1 = True
+                borrar2 = True
+                fline1 = ''
+                fline2 = ''
 
-    with open("test.csv", "r") as f:
-        lines = f.readlines()
+                for c in lines1[i]:
+                    if borrar1:
+                        if c == ',':
+                            borrar1 = False
+                    else:
+                        if c != '\n':
+                            fline1 = fline1 + c
+
+                for c in lines2[i]:
+                    if borrar2:
+                        if c == ',':
+                            fline2 = fline2 + c
+                            borrar2 = False
+                    else:
+                        fline2 = fline2 + c      
+                f.write(fline1+fline2)
+    with open("df3.csv", "r") as f:
+        lines1 = f.readlines()
+    with open("df4.csv", "r") as f:
+        lines2 = f.readlines()
     with open("test.csv", "w") as f:
-        for line in lines:
-            if line.strip("\n") != ",sex1,sex2,sex3,length,diam,height,wholew,shuckedw,visceraw,shellw,rings":
-                borrar = True
-                fline = ''
-                for i in line:
-                    if borrar:
-                        if i == ',':
-                            borrar = False
-                    else:
-                        fline = fline + i
-                f.write(fline)
+        for i in range(len(lines1)):
+            if lines1[i].strip("\n") != ",0,1,2,3,4,5,6,7,8,9":
+                borrar1 = True
+                borrar2 = True
+                fline1 = ''
+                fline2 = ''
 
+                for c in lines1[i]:
+                    if borrar1:
+                        if c == ',':
+                            borrar1 = False
+                    else:
+                        if c != '\n':
+                            fline1 = fline1 + c
+
+                for c in lines2[i]:
+                    if borrar2:
+                        if c == ',':
+                            fline2 = fline2 + c
+                            borrar2 = False
+                    else:
+                        fline2 = fline2 + c      
+                f.write(fline1+fline2)
+    os.remove("df1.csv")
+    os.remove("df2.csv")
+    os.remove("df3.csv")
+    os.remove("df4.csv")
+    
 dataset = pd.read_csv("abalone.csv")
+X = dataset.drop('rings', axis = 1)
 grafiques(dataset)
 dataset=elimOutliers(dataset)
-createTestAndTrain(0.8, dataset)
+createTestAndTrain(0.20, dataset)
+
